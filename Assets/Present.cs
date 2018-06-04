0636bb292ec7bfa7b460b2cc30caadd87cc9c864
dabiscup@microsoft.com <dabiscup@microsoft.com>
2018-06-04 11:48:42 -0500

    corrected notes

---
+++ Assets/Present.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Present : MonoBehaviour {
+
+    public Rigidbody2D m_rb;
+
+    void Start ()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        Vector2 v = new Vector2(Random.Range(-300, -150), Random.Range(150, 400));
+        m_rb.AddForce(v);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Sleigh.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sleigh : MonoBehaviour {
+
+    public Rigidbody2D m_rb;
+
+    void Start ()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("fire");
+
+            Vector2 v = new Vector2(0, 2.74f);
+
+            Instantiate(GameObject.Find("Present").GetComponent<Rigidbody2D>(), v, UnityEngine.Quaternion.identity);
+
+        }
+
+
+    }
+}
